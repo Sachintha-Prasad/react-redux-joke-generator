cdfdb43cf514bf7365d986082840a555e0f319dd
Sachintha Prasad <prasadsachintha1231@gmail.com>
2024-09-17 09:13:14 +0530

    feat: define layout and add joke component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Col, Layout } from "antd"
 import React from "react"
+import JokeContainer from "./features/jokeGenerator/JokeComponent"
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <Layout
+            style={{
+                background: "linear-gradient(90deg,#f6c4ed, #e1dae6)",
+                minHeight: "100vh"
+            }}
+        >
+            <Col
+                xs={{ span: 20, offset: 2 }}
+                sm={{ span: 10, offset: 7 }}
+                xl={{ span: 6, offset: 9 }}
+                style={{
+                    width: "100%",
+                    marginBlock: 120
+                }}
+            >
+                <JokeContainer />
+            </Col>
+        </Layout>
+    )
 }
 
 export default App
